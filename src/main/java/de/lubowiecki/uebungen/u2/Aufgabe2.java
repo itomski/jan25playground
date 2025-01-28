61dad21099bcf8a8353360e6240bcea1ba7112d4
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-01-28 13:07:38 +0100

    Instanz / Static

---
+++ src/main/java/de/lubowiecki/uebungen/u2/Aufgabe2.java
@@ -0,0 +1,13 @@
+package de.lubowiecki.uebungen.u2;
+
+public class Aufgabe2 {
+
+    // Schreibe eine Klasse für ein Fahrzeug
+    // Es enthält ein Kennzeichen, Marke, Modell und die gefahrenen Kilometer
+    // mit einer Methode fahre() kann die Anzahl gefahrener Kilometer verändert werden
+    // dabei wird bei Aufruf der Methode die Strecke in km vorgegeben
+
+    // Instanziere zwei unterschiedliche Fahrzeuge.
+    // Fahre damit etwas rum und gib den Zustand der Fahrzeuge anschließen aus
+
+}
