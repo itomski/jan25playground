31fd7e6c715ed0fb7df70657cdea3727d6026c49
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-01-30 13:52:26 +0100

    Update: Kontrollstrukturen

--- src/main/java/de/lubowiecki/KontrollstrukturenTest1.java
+++ src/main/java/de/lubowiecki/KontrollstrukturenTest1.java
@@ -8,7 +8,22 @@ public class KontrollstrukturenTest1 {
 
     public static void main(String[] args) {
 
+        // printStytem();
+
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Eingabe: ");
+        String input = scanner.next();
+
+        // ifCheck(input);
+
+        // switchCheck(input);
+
+        wieLangeNochBisWE(input);
+
+    }
+
+    private static void printStytem() {
 
         System.out.println(System.getProperties());
         System.out.println(System.getProperty("java.specification.version"));
@@ -18,9 +33,9 @@ public class KontrollstrukturenTest1 {
         // System.out = Ausgabestrom
         // System.err = Ausgabestrom für Fehlermeldungen
         // System.err.println("Ausgabe in rot!");
+    }
 
-        System.out.print("Eingabe: ");
-        String input = scanner.next();
+    private static void ifCheck(String input) {
 
         // == prüft Gleichheit von primitiven Datentypen
         // == prüft bei Objekten, ob es das gleiche Objekt auf dem Heap ist
@@ -39,10 +54,76 @@ public class KontrollstrukturenTest1 {
         else // Bei einer eint´zigen Anweisung können die {...} weggelassen werden
             System.out.println("Bot: He? Verstehe ich nicht!");
 
-
-
 //        if(!input.isEmpty()) { // Prüft, ob irgendetwas eigegeben wurde
 //            System.out.println("Bot: Trotzdem Hallo!");
 //        }
+    }
+
+    private static void switchCheck(String input) {
+
+        // trim entfernt Leerzeichen oder Steuerzeichen an beiden Enden der Zeichenkette
+        switch(input.trim().toLowerCase()) {
+
+            case "hallo":
+                System.out.println("Bot: Moin Moin!");
+                break; // Fehlt ein berak, wird der Folge-Case auch ausgeführt
+
+            case "time":
+                System.out.println(LocalTime.now());
+                break;
+
+            // Jeder case muss einzigartig sein
+            case "date":
+                System.out.println(LocalDate.now());
+                break;
+
+            default:
+                System.out.println("Bot: He? Verstehe ich nicht!");
+
+        }
+    }
+
+    // Seit Java 14 möglich
+    private static void switchCheckNew(String input) {
+
+        // Mögliche Werte
+        // Ganzzahlen (byte, short, int), Strings, char, ENUMs
+
+        // trim entfernt Leerzeichen oder Steuerzeichen an beiden Enden der Zeichenkette
+        switch(input.trim().toLowerCase()) {
+            // -> baut automatisch ein break ein
+            // nach dem -> nur eine Anweisung oder ein Block möglich
+            case "hallo" -> System.out.println("Bot: Moin Moin!");
+            case "time" -> System.out.println(LocalTime.now());
+            case "date" -> { // Block
+                System.out.print("Datum: ");
+                System.out.println(LocalDate.now());
+            }
+            default -> System.out.println("Bot: He? Verstehe ich nicht!");
+        }
+    }
+
+
+    private static void wieLangeNochBisWE(String tag) {
+
+        switch(tag.toUpperCase()) {
+            case "MO":
+                System.out.println("Montag");
+            case "DI":
+                System.out.println("Dienstag");
+            case "MI":
+                System.out.println("Mittwoch");
+            case "DO":
+                System.out.println("Donnerstag");
+            case "FR":
+                System.out.println("Freitag");
+            case "SA":
+            case "SO":
+                System.out.println("Wochenende!");
+                break;
+
+            default:
+                System.out.println("Unbekannt");
+        }
     }
 }
