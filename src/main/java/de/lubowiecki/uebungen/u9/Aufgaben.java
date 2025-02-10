6875f361f8ba65ad52bbcbccbf1303e9f3954495
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-02-10 14:17:29 +0100

    Update: Aufgaben

---
+++ src/main/java/de/lubowiecki/uebungen/u9/Aufgaben.java
@@ -0,0 +1,26 @@
+package de.lubowiecki.uebungen.u9;
+
+public class Aufgaben {
+
+    /*
+    Aufgabe 1:
+    Schreibe eine Methode, die mehrere Zahlen entgegennimmt und eine Summe davon zurückgibt.
+    Und eine zweite gleichnamige Methode mit nur 2 Zahlen. Sie gibt ebenfalls eine Summe zurück.
+     */
+
+    /*
+    Aufgabe 2:
+    Schreibe eine Klasse für einen Taschenrechner mit den 4 Grunrechenarten.
+    Jedes mal, wenn eine Rechnung ausgeführt wird, wird ein Eintrag in ein Protokoll geschrieben.
+    Schreibe eine Methode, mit der dann der Inhalt des Protokolls ausgegeben werden kann.
+    Verwende für das Protokoll einen StringBuilder
+     */
+
+    /*
+    Aufgabe 3:
+    Schreibe eine Klasse für eine Einkaufsliste.
+    Man kann der Liste etwas hinzufügen und sich den Inhalt der Liste anzeigen lassen.
+    Jeder Eintrag wird als unabhängiger String gespeichert.
+    Verwende eine ArrayList.
+     */
+}
