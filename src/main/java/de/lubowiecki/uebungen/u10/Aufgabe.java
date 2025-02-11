00df6dc5987c543e26d6ea6044a5ada0aed87ed0
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-02-11 13:11:18 +0100

    Update: Aufgabe

---
+++ src/main/java/de/lubowiecki/uebungen/u10/Aufgabe.java
@@ -0,0 +1,16 @@
+package de.lubowiecki.uebungen.u10;
+
+public class Aufgabe {
+
+    /*
+    Aufgabe 1
+    Schreibe ein Programm für die Verwaltung einer Bibliothek.
+    Es soll möglich sein, Bücher mit den üblichen Angaben zu erfassen.
+    Die Bücher werden in einer Liste gespeichert.
+    Ein Buch soll über folgene Infos verfügen:
+    ISBN, Autor, Titel, Erscheinungsjahr, Verlag, Beschreibung, Anzahl-Seiten
+
+    Neben dem Erfassen, soll auch die Möglichkeit einer Ausgabe bestehen.
+     */
+
+}
