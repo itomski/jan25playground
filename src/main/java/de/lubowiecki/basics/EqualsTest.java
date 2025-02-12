85b46b936beb7e8422fc5f79d3843da9c1273f2d
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-02-12 10:40:13 +0100

    Update: Equals / HashCode

---
+++ src/main/java/de/lubowiecki/basics/EqualsTest.java
@@ -0,0 +1,58 @@
+package de.lubowiecki.basics;
+
+import java.util.Objects;
+
+public class EqualsTest {
+
+    public static void main(String[] args) {
+
+        String a = "Das ist das Haus von Nikigraus"; // Landet im String-Literal-Pool
+        String b = "Das ist das Haus von Nikigraus"; // Verwendet den Wert aus dem String-Literal-Pool
+        String c = new String("Das ist das Haus von Nikigraus"); // Verwendet NICHT den Pool
+
+        System.out.println(a == b); // Ist es das gleiche Objekt auf dem Heap?
+        System.out.println(a.equals(b)); // Hat es den gleichen Inhalt?
+
+        System.out.println();
+
+        System.out.println(a == c); // Ist es das gleiche Objekt auf dem Heap?
+        System.out.println(a.equals(c)); // Hat es den gleichen Inhalt?
+
+        System.out.println();
+
+        NumberBox b1 = new NumberBox(15);
+        NumberBox b2 = new NumberBox(15);
+        //NumberBox b2 = null;
+
+        System.out.println(b1 == b2); // Ist es das gleiche Objekt auf dem Heap?
+        System.out.println(b1.equals(b2)); // Hat es den gleichen Inhalt?
+        System.out.println(b1.hashCode());
+        System.out.println(b2.hashCode());
+    }
+}
+
+class NumberBox {
+
+    private int nr;
+
+    public NumberBox(int nr) {
+        this.nr = nr;
+    }
+
+    public int getNr() {
+        return nr;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        NumberBox numberBox = (NumberBox) o;
+        return nr == numberBox.nr;
+    }
+
+    // equals und hashCode MÜSSEN auf gleichen Eigenschaften basieren
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(nr);
+    }
+}
---
+++ src/main/java/de/lubowiecki/uebungen/u11/Aufgaben.java
@@ -0,0 +1,13 @@
+package de.lubowiecki.uebungen.u11;
+
+public class Aufgaben {
+
+    /*
+    Entwerfe zwei Klassen (z.B. Kunde und Film)
+    Instanziere die Klassen jeweils 2x und vergleiche sie mit equals.
+    Sorge dafür, dass die Instanzen beim gleichen Inhalt ein true beim equals-Vergleich ergeben.
+    Gleiche Inhalt soll auch zu gleichen hashCode führen.
+     */
+
+
+}
