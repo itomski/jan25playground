99d94ba3138d388064a2f97776d5a04bf56e1d11
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-02-14 11:53:49 +0100

    Update: Generics und JavaDocs

---
+++ src/main/java/de/lubowiecki/time/Formats.java
@@ -0,0 +1,18 @@
+package de.lubowiecki.time;
+
+public interface Formats {
+
+    public static final String DATE_FMT = "dd.MM.yyyy";
+    public static final String TIME_FMT = "HH:mm:ss";
+}
+
+// final für Klassen = Von dieser Klasse kann man nicht erben
+//public final class Formats {
+//
+//    public static final String DATE_FMT = "dd.MM.yyyy";
+//    public static final String TIME_FMT = "HH:mm:ss";
+//
+//    // privater Konstruktor = Es können keine Objekte von diesr Klasse gebaut werden
+//    private Formats() {
+//    }
+//}
---
+++ src/main/java/de/lubowiecki/time/TimeTest.java
@@ -0,0 +1,13 @@
+package de.lubowiecki.time;
+
+public class TimeTest {
+
+    public static void main(String[] args) {
+
+        // Interface kann nicht instanziert werden
+        // Formats fmt = new Formats(); // Error
+        System.out.println(Formats.DATE_FMT);
+
+        String s;
+    }
+}
