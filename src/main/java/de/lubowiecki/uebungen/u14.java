8dab658a442001b0cbdd5f78bdf1bd431178d11b
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-02-18 13:16:33 +0100

    Update: Exceptions

---
+++ src/main/java/de/lubowiecki/uebungen/u14.java
@@ -0,0 +1,20 @@
+package de.lubowiecki.uebungen;
+
+public class u14 {
+
+    /*
+    Schreibe ein Programm zur Simmulation eines Staubsaugers.
+    Der Staubsauger wird durch eine eigene Klasse beschrieben.
+    Er verfügt über einen Akku für 20 Minuten.
+    Er kann ein- und ausgeschaltet werden
+    Er hat einen Beutel für 40 Minuten normales saugen.
+    Wenn der Staubsauger an ist, kann man mit der Methode saugen(int min) die Anzahl von Minuten vorgeben.
+    Die Ausführung der Methode führt dazu, dass der Akku verbraucht wird und der Beutel gefüllt wird.
+    Über zusätzliche Methoden kann der Beutel geleert werden und der Akku aufgeladen werden.
+    Ist eine Aktion nicht möglich, schmeißen die Methoden passende Exceptions.
+    Diese führen zu passenden Anzeigen auf der Konsole.
+    Viel Spaß
+    */
+
+
+}
