9b4e7b1767be18ea8bdd227416e98002532ba2f7
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-02-20 13:42:50 +0100

    Update: Patterns, Singleton

--- src/main/java/de/lubowiecki/patterns/singleton/SingletonTest.java
+++ src/main/java/de/lubowiecki/patterns/singleton/SingletonTest.java
@@ -1,39 +1,87 @@
 package de.lubowiecki.patterns.singleton;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class SingletonTest {
 
     public static void main(String[] args) {
 
-        Logger.setDebugMode(true);
-
+        // Logger logger = new Logger(); // Error: Konstruktor ist private
+        // getInstance() liefert das Logger Objekt (nur 1x pro Programm verfügbar)
+        Logger.getInstance().setDebugMode(false);
         machWas();
         machWasAnderes();
+
+        System.out.println("---------------");
+
+        Logger.getInstance().print(true);
+
     }
 
     public static void machWas() {
         //... Irgendwas erledigen
-        Logger.log("machWas ausgeführt");
+        Logger.getInstance().log("machWas ausgeführt");
     }
 
     public static void machWasAnderes() {
         //... Irgendwas erledigen
-        Logger.log("machWasAnderes ausgeführt");
+        Logger.getInstance().log("machWasAnderes ausgeführt");
     }
 }
 
+// Singleton: Es darf nur einen geben
 class Logger {
 
-    private static boolean debug;
+    private boolean debug;
 
-    public static void log(String msg) {
-        if(debug) {
+    // Hier werden die Meldungen gesammelt, wenn sie nicht ausgegeben werden
+    private final StringBuilder protokoll = new StringBuilder();
+
+    // Format für das Datum, welches für den Dateinamen verwendet wird
+    private static final DateTimeFormatter FMT = DateTimeFormatter.ofPattern("ddMMyyyy_HHmmss");
+
+    private static final Logger instance = new Logger();
+
+    private Logger() {
+    }
+
+    public static Logger getInstance() {
+        return instance;
+    }
+
+    public void log(String msg) {
+        msg = LocalDateTime.now() + ": " + msg;
+
+        if(debug) { // Ausgabe auf der Konsole
             System.out.println(LocalDateTime.now() + ": " + msg);
+        }
+        else { // Schreiben in den StringBuilder für spätere Ausgabe
+            protokoll.append(msg).append("\n");
         }
     }
 
-    public static void setDebugMode(boolean debug) {
-        Logger.debug = debug;
+    public void setDebugMode(boolean debug) {
+        this.debug = debug;
+    }
+
+    public void print(boolean toFile) {
+        if(toFile) {
+            // Dateiname
+            Path pfad = Paths.get(LocalDateTime.now().format(FMT) + ".txt");
+            try {
+                Files.writeString(pfad, protokoll); // Schreiben in die Datei
+            }
+            catch (IOException e) {
+                System.out.println("Problem beim Schreiben"); // Wenn nicht geschrieben werden kann
+            }
+        }
+        else {
+            System.out.println(protokoll); // Ausgabe auf der Konsole
+        }
     }
 }
