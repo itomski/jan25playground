a493a525a5c8934de166972367a1390dc4510d6c
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-02-21 13:23:09 +0100

    Patterns: Aufgabe

--- src/main/java/de/lubowiecki/uebungen/u16/Aufgabe.java
+++ src/main/java/de/lubowiecki/uebungen/u16/Aufgabe.java
@@ -3,8 +3,28 @@ package de.lubowiecki.uebungen.u16;
 public class Aufgabe {
 
     /*
+    Übung 1
     Schreibe ein Programm, dass eine Eingabe für ein Datum und Uhrzeit als String von der
     Konsole entgegennimmt und es richtig zu einem LocalDateTime parst.
+     */
+
+     /*
+    Übung 2
+    Schreibe ein Programm für die Zeiterfassung.
+    Du kannst das Programm mit JavaFX oder als Konsolenprogramm starten
+
+    Es sind folgende Befehle möglich:
+        start = Beginn der Zeiterfassung
+        ende = Ende der Zeiterfassung
+
+    Wird die Zeiterfassung beendet wird die Zeit in Stunden und Minuten angezeigt.
+
+
+    Erweiterung 1:
+    Man kann die Zeierfassung mit dem Befehl start_pause unterbrechen und mit ende_pause fortsetzen
+
+    Erweiterung 2:
+    Schreibe die Zeiterfassung als ein Singleton
      */
 
 }
