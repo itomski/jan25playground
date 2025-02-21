397489a6cb9c0b9d766a799bd98d825a8b831a2d
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-02-21 09:41:20 +0100

    Update: Time/Date API

---
+++ src/main/java/de/lubowiecki/uebungen/u16/Aufgabe.java
@@ -0,0 +1,10 @@
+package de.lubowiecki.uebungen.u16;
+
+public class Aufgabe {
+
+    /*
+    Schreibe ein Programm, dass eine Eingabe für ein Datum und Uhrzeit als String von der
+    Konsole entgegennimmt und es richtig zu einem LocalDateTime parst.
+     */
+
+}
