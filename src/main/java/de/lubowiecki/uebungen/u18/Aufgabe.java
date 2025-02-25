87ab594c926a216e47ce918ade3e8295667946fd
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-02-25 13:53:46 +0100

    Aufgabe

---
+++ src/main/java/de/lubowiecki/uebungen/u18/Aufgabe.java
@@ -0,0 +1,19 @@
+package de.lubowiecki.uebungen.u18;
+
+public class Aufgabe {
+
+    /*
+    Aufgabe 1:
+    Erzeuge ein neues JavFX Projekt.
+    Passe die start-Methode so an, dass du das folgende Tutorial durcharbeiten kannst:
+    https://falconbyte.net/javafx-bedienelemente
+     */
+
+    /*
+    Aufgabe 2
+    Bilde mit einer JavaFX GUI den Controller einer Spieleconsole nach.
+    Schreibe passende methoden, die beim betätigen der Buttons passende Ausgaben in ein Label vornehmen
+    Passende Ausgaben könnten sein: Up, Down, Left, Right, Jump etc.
+     */
+
+}
