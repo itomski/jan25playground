2fb0489d4bf731670e02b0b9811115d0d85bb304
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-02-25 09:19:34 +0100

    JDBC: PreparedStatements

--- src/main/java/de/lubowiecki/datenbanken/CrudTest.java
+++ src/main/java/de/lubowiecki/datenbanken/CrudTest.java
@@ -2,6 +2,7 @@ package de.lubowiecki.datenbanken;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 public class CrudTest {
 
@@ -13,15 +14,33 @@ public class CrudTest {
 
         try {
 
-            if(crud.insert(k1)) {
-                System.out.println("Wurde gespeichert!");
+            // Einfügen
+//            if(crud.insert(k1)) {
+//                System.out.println("Wurde gespeichert!");
+//            }
+//            else {
+//                System.out.println("Konnte nicht gespeichert werden!");
+//            }
+//
+//            System.out.println();
+
+
+            // Einen abfragen
+            Optional<Kunde> opt = crud.findById(1);
+            //opt.ifPresent(k -> System.out.println(k));
+
+            if(opt.isPresent()) {
+                Kunde k = opt.get(); // get liefert das Objekt, das Optional liegt
+                k.setNachname("Rogers");
+                k.setAktiv(true);
+                // Datensatz ändern
+                crud.update(k);
             }
             else {
-                System.out.println("Konnte nicht gespeichert werden!");
+                System.out.println("Kein passender Kunde gefunden.");
             }
 
-            System.out.println();
-
+            // Alle abfragen
             for(Kunde k : crud.findAll()) {
                 System.out.println(k.getId() + ": " + k.getVorname() + " " +
                         k.getNachname() + ", " + ((k.isAktiv()) ? "aktiv" : "nicht aktiv"));
--- src/main/java/de/lubowiecki/datenbanken/KundenCrud.java
+++ src/main/java/de/lubowiecki/datenbanken/KundenCrud.java
@@ -3,12 +3,10 @@ package de.lubowiecki.datenbanken;
 // ORM Objekt-Relationales-Mapping: Übersetzt Relationale Daten aus der Datenbank in Java Objekte
 // und umgekehrt
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 // CRUD = Create Read Update Delete
 public class KundenCrud {
@@ -30,19 +28,73 @@ public class KundenCrud {
         }
     }
 
-    public boolean insert(Kunde kunde) throws SQLException {
+    // Bei Methoden, wo es vorkommen kann, dass nichts passendes zurückgegeben werden kann
+    // verwendet man lieber ein Optional statt null
+    public Optional<Kunde> findById(int id) throws SQLException {
 
         try(Connection verbindung = DbUtility.getConnection();
             Statement stmt = verbindung.createStatement()) {
 
-            // INSERT INTO " + TABLE + " (id, vorname, nachname) VALUES(NULL, 'Bruce', 'Banner')
-            final String SQL = "INSERT INTO " + TABLE + " (vorname, nachname) " +
-                                    "VALUES('%s', '%s')"; // %s ist ein Platzhalter für einen String
+            ResultSet daten = stmt.executeQuery("SELECT * FROM " + TABLE + " WHERE id = " + id);
 
-            // executeUpdate liefert die Anzahl der Zeilen die geändert/hinzugefügt wurden
-            return stmt.executeUpdate(String.format(SQL, kunde.getVorname(), kunde.getNachname())) > 0; // Das Einfügen ist ein Update des Inhalts einer Tabelle
+            if(daten.next()) {
+                return Optional.of(create(daten)); // Optional von Kunde
+            }
+            return Optional.empty(); // Leeres Optional, wenn kein Kunde gefunden
         }
     }
+
+    public boolean insert(Kunde kunde) throws SQLException {
+
+        // Hier ist eine SQL-Injection möglich
+//        try(Connection verbindung = DbUtility.getConnection();
+//            Statement stmt = verbindung.createStatement()) {
+//
+//            // INSERT INTO " + TABLE + " (id, vorname, nachname) VALUES(NULL, 'Bruce', 'Banner')
+//            final String SQL = "INSERT INTO " + TABLE + " (vorname, nachname) " +
+//                                    "VALUES('%s', '%s')"; // %s ist ein Platzhalter für einen String
+//
+//            // executeUpdate liefert die Anzahl der Zeilen die geändert/hinzugefügt wurden
+//            return stmt.executeUpdate(String.format(SQL, kunde.getVorname(), kunde.getNachname())) > 0; // Das Einfügen ist ein Update des Inhalts einer Tabelle
+//        }
+
+        // Sicher gegen SQL-Injection
+
+        final String SQL = "INSERT INTO " + TABLE + " (vorname, nachname) VALUES(?, ?)";
+
+        // prepareStatement(SQL) : Übergibt ein Template für den Befehl an die Datenbank
+        // Daten für ? werden später nur als Daten und nicht als Befehle interpretiert
+        try(Connection verbindung = DbUtility.getConnection();
+            PreparedStatement stmt = verbindung.prepareStatement(SQL)) {
+
+            // Die Fragezeichen im Template werden durch konkrete Werte ersetzt
+            stmt.setString(1, kunde.getVorname()); // 1 ist die Nr. des ?
+            stmt.setString(2, kunde.getNachname());
+
+            // Stringinhalte die, die ? ersetzen werden NIE als Befehle ausgewertet
+
+            return stmt.executeUpdate() > 0;
+        }
+    }
+
+    public boolean update(Kunde kunde) throws SQLException {
+
+        final String SQL = "UPDATE " + TABLE + " SET vorname = ?, nachname = ?, aktiv = ? WHERE id = ?";
+
+        try(Connection verbindung = DbUtility.getConnection();
+            PreparedStatement stmt = verbindung.prepareStatement(SQL)) {
+
+            // Die Fragezeichen im Template werden durch konkrete Werte ersetzt
+            stmt.setString(1, kunde.getVorname()); // 1 ist die Nr. des ?
+            stmt.setString(2, kunde.getNachname());
+            stmt.setBoolean(3, kunde.isAktiv());
+            stmt.setInt(4, kunde.getId());
+
+            // Stringinhalte die, die ? ersetzen werden NIE als Befehle ausgewertet
+            return stmt.executeUpdate() > 0;
+        }
+    }
+
 
     private Kunde create(ResultSet zeile) throws SQLException {
         Kunde k = new Kunde();
