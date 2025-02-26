e6d7409d3bacd601e697f769c8220286a65853c6
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-02-26 12:06:28 +0100

    SQLite

---
+++ src/main/java/de/lubowiecki/datenbanken/SqLiteTest.java
@@ -0,0 +1,27 @@
+package de.lubowiecki.datenbanken;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SqLiteTest {
+
+    public static void main(String[] args) {
+
+        try(Connection verbindung = SQLiteUtils.getConnection();
+            Statement stmt = verbindung.createStatement()) {
+
+            // Erzeugung der Tabelle direkt in Java
+            stmt.execute("CREATE TABLE IF NOT EXISTS benutzer (" +
+                    "id INTEGER," +
+                    "vorname TEXT NOT NULL," +
+                    "nachname TEXT NOT NULL," +
+                    "PRIMARY KEY(id AUTOINCREMENT))");
+
+            System.out.println("Fertig!");
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
