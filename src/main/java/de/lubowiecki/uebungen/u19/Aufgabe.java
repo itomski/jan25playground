1a203f218d4675350f59f93fc3aaa73c76138a80
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-02-26 14:45:13 +0100

    Aufgabe

---
+++ src/main/java/de/lubowiecki/uebungen/u19/Aufgabe.java
@@ -0,0 +1,31 @@
+package de.lubowiecki.uebungen.u19;
+
+public class Aufgabe {
+
+    /*
+    Schreibe ein Programm, das es erlaubt Kochrezepte zu verwalten:
+    eingeben, speichern, suchen
+    Im Hintergrund soll eine kleine Datenbank hängen.
+
+    Dazu gehört:
+
+- Features auflisten, ca. 1 Seite Pflichtenheft schreiben
+
+- Klassendiagramm: welche Felder, welche Methoden brauche meine Klassen?
+    Denkt dabei auch an das MVC-Pattern
+
+- GUI-Design: welche GUIs brauche ich? Wie sollen die aussehen? Macht eine Skizze oder ein MockUp davon
+    Entwerfe später die GUIs im SceneBuilder
+
+- Teste deinen Code, am besten jede Methode mindestens einmal ausführen lassen, bevor du die nächste anfängst!
+
+- Datenbankdiagramm erstellen, bevor du die Datenbank anlegst.
+    Frag dich: Was muss ich wirklich speichern?
+
+- SQL-Statements erst einmal auf der Datenbank ausprobieren, bevor du sie in deinen Code einbaust.
+
+- Objekte so erstellen, dass du beim Auswerten des ResultSets hinten ein einzelnes Objekt zurückgeben kannst
+    Mapping der relationalen Daten in Objekte
+    */
+
+}
